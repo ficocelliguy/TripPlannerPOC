f627e39fb0711d4519d8c3ae066cf0ded32fe1ec
Michael Ficocelli <ficocemt@gmail.com>
2023-06-21 14:34:59 -0400

    [PATCH] Separate client and server tsconfig, lint, and build

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,25 +1,26 @@
 const dbUri = 'http://localhost:9090'
 
 fetch(dbUri, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ query: `
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({
+    query: `
     query {
       allData {
         name
         id
       }
     }`
-    }),
+  })
 })
-    .then(res => res.json())
-    .then(res => {
-        console.log(res.data);
-        document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  .then(async res => await res.json())
+  .then(res => {
+    console.log(res.data)
+    // eslint-disable-next-line  @typescript-eslint/no-non-null-assertion
+    document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
           <div>
             <h1>Vite + TypeScript</h1>
             <p>${JSON.stringify(res.data)}</p>
           </div>
         `
-    });
-
+  }).catch(e => { console.error(e) })
---
+++ src/server/index.ts
@@ -0,0 +1,11 @@
+import { ApolloServer } from 'apollo-server'
+import { typeDefs } from './schemas/schema.js'
+import { resolvers } from './resolvers/resolvers.js'
+
+const port = process.env.PORT ?? 9090
+
+const server = new ApolloServer({ resolvers, typeDefs })
+
+server.listen({ port },
+  () => { console.log(`Server runs at: http://localhost:${port}`) }
+).catch(e => { console.log(e) })
