2913844d70a1201eb26687120c532f196b2cb671
Michael Ficocelli <ficocemt@gmail.com>
2023-07-10 15:43:42 -0400

    [PATCH] Tailwind css support

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+export default {
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+    },
+    to: 'dist/client/main.css',
+    from: '.src/client/styles/main.css'
+}
--- src/client/main.ts
+++ src/client/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import App from "./components/App.vue";
+import "./styles/main.css";
 
 const vm = createApp(App);
 
